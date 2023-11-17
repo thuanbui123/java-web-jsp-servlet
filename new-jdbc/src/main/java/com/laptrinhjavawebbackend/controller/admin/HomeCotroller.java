079d0cf24287fa38f6a0da461ee463f6879f853a
Thuanbui <thuanbui31819@gmail.com>
2023-11-17 17:00:55 +0700

    init project

---
+++ new-jdbc/src/main/java/com/laptrinhjavawebbackend/controller/admin/HomeCotroller.java
@@ -0,0 +1,24 @@
+package com.laptrinhjavawebbackend.controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/admin-home"})
+public class HomeCotroller extends HttpServlet {
+
+	private static final long serialVersionUID = -4389051559967945564L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+}
---
+++ new-jdbc/src/main/java/com/laptrinhjavawebbackend/controller/web/HomeCotroller.java
@@ -0,0 +1,31 @@
+package com.laptrinhjavawebbackend.controller.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.laptrinhjavawebbackend.model.UserModel;
+
+@WebServlet(urlPatterns = {"/trang-chu"})
+public class HomeCotroller extends HttpServlet {
+
+	private static final long serialVersionUID = -4389051559967945564L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UserModel userModel = new UserModel();
+		userModel.setFullName("Bùi Văn Thuân");
+		request.setAttribute("model", userModel);
+		RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
+		rd.forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+}
